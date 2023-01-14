6a7bfc0d6344ec499271e507fc1b770133c791df
Jiseok CHOI <jiseok.dev@gmail.com>
2023-01-14 19:33:09 +0900

    Add `sqlite`, `sqllogictest` dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use sqlite::State;
+
 fn main() {
-    println!("Hello, world!");
+    let connection = sqlite::open(":memory:").unwrap();
+
+    connection.execute("CREATE TABLE Foo (name Text)").unwrap();
+    connection
+        .execute("INSERT INTO Foo (name) VALUES ('bar')")
+        .unwrap();
+
+    let query = "SELECT * FROM Foo";
+    let mut statement = connection.prepare(query).unwrap();
+
+    while let Ok(State::Row) = statement.next() {
+        println!("name = {}", statement.read::<String, _>("name").unwrap());
+    }
 }
