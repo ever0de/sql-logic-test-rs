4520a677509d36d1ab750a1e42fafd35b3e3d315
Jiseok CHOI <jiseok.dev@gmail.com>
2023-01-14 20:52:46 +0900

    Change `IntoEyre` to `WrapErr`

--- src/std_ext.rs
+++ src/std_ext.rs
@@ -1,9 +1,19 @@
-pub trait IntoEyre<T> {
+use std::fmt::Display;
+
+pub trait EyreExt<T> {
     fn into_eyre(self) -> eyre::Result<T>;
+
+    fn wrap_err<Msg: Display + Send + Sync + 'static>(self, msg: Msg) -> eyre::Result<T>;
 }
 
-impl<T, E: Into<eyre::ErrReport>> IntoEyre<T> for Result<T, E> {
+impl<T, E: std::error::Error + Into<eyre::ErrReport> + Send + Sync + 'static> EyreExt<T>
+    for Result<T, E>
+{
     fn into_eyre(self) -> eyre::Result<T> {
         self.map_err(Into::into)
+    }
+
+    fn wrap_err<Msg: Display + Send + Sync + 'static>(self, msg: Msg) -> eyre::Result<T> {
+        eyre::WrapErr::wrap_err(self, msg)
     }
 }
