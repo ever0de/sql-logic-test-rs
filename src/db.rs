d49b21ae1bc6ce532f34b3b0581d638138d4b4a1
Jiseok <jiseok.dev@gmail.com>
2023-01-14 21:23:07 +0900

    Separate methods `select` and `select_with_fields`

--- src/db.rs
+++ src/db.rs
@@ -50,7 +50,7 @@ impl Sqlite {
             .wrap_err(format!("failed to prepare statement, sql: {sql}"))
     }
 
-    pub fn select<Sql: AsRef<str>, Fields: AsRef<[&'static str]>>(
+    pub fn select_with_fields<Sql: AsRef<str>, Fields: AsRef<[&'static str]>>(
         &self,
         sql: Sql,
         fields: Fields,
@@ -60,7 +60,7 @@ impl Sqlite {
 
         let mut rows = vec![];
         loop {
-            if let State::Done = stmt.next()? {
+            if let State::Done = stmt.next().wrap_err("failed statment next")? {
                 return Ok(rows);
             }
 
@@ -70,6 +70,32 @@ impl Sqlite {
                     .read::<String, _>(field.as_ref())
                     .wrap_err(format!("failed read, field: {field} from sql: {sql}"))?;
                 let record = Record::new(field.to_string(), value);
+                row.push(record);
+            }
+
+            rows.push(row)
+        }
+    }
+
+    pub fn select<Sql: AsRef<str>>(&self, sql: Sql) -> eyre::Result<Vec<Row>> {
+        let sql = sql.as_ref();
+        let mut stmt = self.prepare(sql)?;
+
+        let mut rows = vec![];
+        loop {
+            if let State::Done = stmt.next().wrap_err("failed statment next")? {
+                return Ok(rows);
+            }
+
+            let mut row = Row::new();
+            for i in 0..stmt.column_count() {
+                let label = stmt
+                    .column_name(i)
+                    .wrap_err(format!("failed column_name, index: {i}"))?;
+                let value = stmt
+                    .read::<String, _>(i)
+                    .wrap_err(format!("failed read, index: {i}"))?;
+                let record = Record::new(label.to_string(), value);
                 row.push(record);
             }
 
@@ -96,8 +122,7 @@ impl Default for Sqlite {
 mod tests {
     use super::*;
 
-    #[test]
-    fn select() {
+    fn setup_sqlite() -> Sqlite {
         let sqlite = Sqlite::new();
         sqlite
             .execute("CREATE TABLE test (id INTEGER PRIMARY KEY, name TEXT)")
@@ -105,9 +130,33 @@ mod tests {
         sqlite
             .execute("INSERT INTO test (name) VALUES ('test')")
             .unwrap();
+        sqlite
+    }
+
+    #[test]
+    fn select_with_fields() {
+        let sqlite = setup_sqlite();
 
         let fields = ["id", "name"];
-        let rows = sqlite.select("SELECT * FROM test", fields).unwrap();
+        let rows = sqlite
+            .select_with_fields("SELECT * FROM test", fields)
+            .unwrap();
+        assert_eq!(rows.len(), 1);
+        assert_eq!(
+            rows[0].record[0],
+            Record::new("id".to_owned(), "1".to_owned())
+        );
+        assert_eq!(
+            rows[0].record[1],
+            Record::new("name".to_owned(), "test".to_owned())
+        );
+    }
+
+    #[test]
+    fn select() {
+        let sqlite = setup_sqlite();
+
+        let rows = sqlite.select("SELECT * FROM test").unwrap();
         assert_eq!(rows.len(), 1);
         assert_eq!(
             rows[0].record[0],
